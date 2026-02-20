80b8b610d19b524071a6c33fd280773892738abc
Aquizu Mariano <marianoaquizu@hotmail.com>
2026-02-20 10:51:50 +0100

    Cambios realizados en src/App.jsx:

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
-import Navbar from './components/navbar/index'
+import { useState } from 'react';
+import { useEffect } from 'react';
 import './App.css'
 
-function App () {
+
+function App() {
+
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    const getPokemons = async () => {
+      const res = await fetch("https://pokeapi.co/api/v2/pokemon/");
+      const listPokemons = await res.json();
+      const { results } = listPokemons;
+      setPokemons(results);
+    }
+
+
+    getPokemons()
+  }, []);
 
   return (
-    <>
-      <Navbar />
-    </>
+    <div>
+      <h1>Pokedex</h1>
+      {pokemons.map(pokemon => { <h3 key={pokemon.name}>{pokemon.name}</h3> }) }
+  </div>
   )
-}
+};
 
 export default App;
